bed4fa23fca138c8b7e988695c79b817f6d915c9
Vasya <oiuy78@yandex.ru>
2023-04-01 06:09:23 +0300

    Add test and Inline mode

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from aiogram import Bot, Dispatcher, executor, types
-import os
-from dotenv import load_dotenv
+import asyncio
+import random
+import sqlite3
+import time
+import hashlib
+import aioschedule
+import schedule as schedule
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from random import choice
 
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+from config import API_TOKEN, CAPTION
 from markups import *
 
-load_dotenv()
 
-bot = Bot(os.getenv('API_TOKEN'), parse_mode='HTML')
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+bot = Bot(API_TOKEN, parse_mode='HTML')
+dp = Dispatcher(bot, storage=storage)
 
 
 async def on_startup(_):
@@ -18,33 +29,39 @@ async def on_startup(_):
 
 HELP_TEXT = """
 <b>/rate</b> - <em>Пооценивать фотки</em>
+<b>/test</b> - <em>Пройти тест на IQ</em>
 """
 
-PHOTOS = ['https://shutniks.com/wp-content/uploads/2020/01/devushka_s_kameroy_14_13035223.jpg',
-          'https://www.sostav.ru/blogs/images/feeds/17/32441.jpg',
-          'https://klike.net/uploads/posts/2022-09/1663154659_g-9.jpg',
-          'https://microsac.es/wp-content/uploads/2019/06/8V1z7D_t20_YX6vKm.jpg',
-          'https://www.blackpantera.ru/upload/iblock/b0e/Znachenie-imeni-Aslan.jpg',
-          'https://mysekret.ru/wp-content/uploads/2022/01/reinhart-julian-wxm465om4j4-unsplash.jpg',
-          'https://myfreesoft.ru/wp-content/uploads/2019/03/prephoto1.jpg',
-          'https://api.innogest.ru/sites/default/files/news/images/179246821_1450.jpg',
-          'https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663131459_22-mykaleidoscope-ru-p-veselie-druzya-krasivo-24.jpg',
-          'https://sun9-60.userapi.com/s/v1/if2/QQOSnilTIj128JGeAEtw1wq2L1bhMadQuSr2pf0Nks8Ov1NzTB-l31FXnOam-CxWOVThUW72mfgKyrCxd8FXdOZ_.jpg?size=1280x853&quality=95&type=album',
-          'https://paypress.ru/uploads/2020/08/1598242178-blonde-629726-1920.jpeg',
-          'https://android-obzor.com/wp-content/uploads/2022/03/chto-nuzhno-znat-pro-introverta-yesli-on-vash-partner_1-1.jpg',
-          'https://cdn.fishki.net/upload/post/201408/25/1297423/20.jpg',
-          'https://vsegda-pomnim.com/uploads/posts/2022-04/1649126083_20-vsegda-pomnim-com-p-samie-krasivie-peizazhi-prirodi-foto-23.jpg',
-          'https://yobte.ru/uploads/posts/2019-11/avstralijki-61-foto-25.jpg',
-          'https://boliviamia.net/Images/ArticlePhotos/Bolivia-experiencias-imperdibles-02.jpg']
+
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    # conn = sqlite3.connect('db.sqlite3')
+    # cur = conn.cursor()
+    # cur.execute('CREATE TABLE IF NOT EXISTS users '
+    #             '(id integer not null unique, '
+    #             'user_id integer not null unique,'
+    #             'name varchar(50) not null,'
+    #             'PRIMARY KEY(id autoincrement))')
+    # conn.commit()
+    # cur.close()
+    # conn.close()
+
+    id_chat = message.chat.id
+    first_name = message.from_user.first_name
     await message.answer(f'Привет, <b>{message.from_user.first_name}!👋</b>\n\n'
                          f'Этого бота создал - @Solevaaaya - топ 2 БО России на минутучку <em>(да-да, 35 ранг)</em>\n\n'
                          f'Этот бот умеет работать с базой данных\n'
                          f'Чтобы увидеть все команды нажми - /help',
                          reply_markup=markup_start)
+
+    conn = sqlite3.connect('db.sqlite3')
+    cur = conn.cursor()
+    cur.execute('INSERT OR IGNORE INTO users (user_id, name) VALUES ("%s", "%s")' % (id_chat, first_name))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 @dp.message_handler(commands=['help'])
@@ -54,24 +71,102 @@ async def help_comm(message: types.Message):
 
 @dp.message_handler(commands=['rate'])
 async def rate_photo(message: types.Message):
-    await message.answer_photo(choice(PHOTOS), reply_markup=markup_photo)
+    photo = choice(list(CAPTION.keys()))
+    await message.answer_photo(photo, reply_markup=markup_photo, caption=CAPTION[photo])
 
 
-@dp.callback_query_handler(lambda call: types.CallbackQuery)
-async def callback(call):
+# class ClientStatesGroup(StatesGroup):
+#     question = State()
+#     question2 = State()
+
+
+@dp.message_handler(commands=['test'])
+async def test_cmd(message: types.Message):
+    await message.answer('Тест на IQ🧐', reply_markup=markup_test)
+
+
+async def test_done(message: types.Message):
+    time.sleep(2)
+    await message.answer('Итак, подводим итоги...')
+    time.sleep(4)
+    await message.answer('<em>Сбор данных...</em>')
+    time.sleep(3)
+    await message.answer('<em>Загрузка...</em>')
+    time.sleep(3)
+    await message.answer(f'<b>Твой IQ => {random.randint(-10, 10)}</b>\n\nПоздравлю! Отличный результат! Ты этого заслуживаешь!💪')
+    time.sleep(2)
+    await message.answer_sticker('CAACAgIAAxkBAAEIaAdkJ4CLyfWswB9y8O7G4QfQs8LwagACcgEAAiI3jgTwHKSRbRIvOi8E')
+    time.sleep(2)
+    await message.answer_sticker('CAACAgIAAxkBAAEIaAtkJ4DJLAGDPvZQvGxI272XdLruRAACsgEAAhZCawqceQABhqhR6VIvBA')
+    return
+
+
+@dp.callback_query_handler(lambda call: call.data == 'holdik' or call.data == 'vasyadasher' or call.data == 'mma' or call.data == 'antinub' or call.data == 'cancel')
+async def callback_q1(call: types.CallbackQuery, state: FSMContext):
+    if call.data == 'vasyadasher':
+        await call.message.edit_text('Красава, бро! Конечно же VasyaDasher, тут без сомнений😎\n\n'
+                                     '<b>Второй вопрос:</b><em>Сколько мне лет?</em>',
+                                     reply_markup=markup_test_question2)
+        await call.answer()
+    if call.data == 'holdik' or call.data == 'mma' or call.data == 'antinub':
+        await call.message.edit_text('НЕВЕРНО!😡 Как можно не знать это!???\n\n'
+                                     '<b>Второй вопрос:</b><em>Сколько мне лет?</em>',
+                                     reply_markup=markup_test_question2)
+        await call.answer()
+    if call.data == 'cancel':
+        await call.message.delete()
+        await bot.delete_message(call.message.chat.id, call.message.message_id - 1)
+    await call.answer('Отменено')
+
+
+@dp.callback_query_handler(lambda call: call.data == '20' or call.data == '87' or call.data == '148' or call.data == '16')
+async def callback_q2(call: types.CallbackQuery):
+    if call.data == '16':
+        await call.message.edit_text('Уоу Уоу! Это верно!🥳 Хотя ты почти ошибся')
+        await call.answer()
+        await test_done(call.message)
+    else:
+        await call.message.edit_text('Эхх, к сожалению, это не правильно\nНо ты был близок😉')
+        await call.answer()
+        await test_done(call.message)
+
+
+@dp.callback_query_handler(lambda call: call.data == 'dislike' or call.data == 'like')
+async def callback_rate(call: types.CallbackQuery):
     if call.data == 'like':
         await rate_photo(call.message)
-    elif call.data == 'dislike':
+        await call.answer()
+    if call.data == 'dislike':
         await rate_photo(call.message)
+        await call.answer()
+
+
+@dp.callback_query_handler(lambda call: call.data == 'go')
+async def callback_test_start(call: types.CallbackQuery):
+    await call.message.edit_text('<b>Первый вопрос:</b>\n\n<em>Кто лучший БО мира в 2022 году?</em>',
+                                 reply_markup=markup_test_question)
 
 
 @dp.message_handler(content_types='text')
-async def text(message: types.Message):
+async def text_cmd(message: types.Message):
     if message.text == 'Все команды':
         await help_comm(message)
     elif message.text == 'Заценить фотки':
         await rate_photo(message)
 
+
+@dp.inline_handler()
+async def inline_answer(inline: types.InlineQuery) -> None:
+    text = inline.query or 'echo'
+    input_content = types.InputTextMessageContent(text)
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    item = types.InlineQueryResultArticle(
+        input_message_content=input_content,
+        id=result_id,
+        title=text
+    )
+
+    await bot.answer_inline_query(inline_query_id=inline.id, results=[item], cache_time=1)
 
 if __name__ == "__main__":
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
--- markups.py
+++ markups.py
@@ -15,5 +15,31 @@ markup_start.add(but_photo, but_help, )
 markup_photo = InlineKeyboardMarkup(row_width=2)
 bt1 = InlineKeyboardButton('👍', callback_data='like')
 bt2 = InlineKeyboardButton('👎', callback_data='dislike')
-bt3 = InlineKeyboardButton('💌', url='t.me/iBekZzZ')
+bt3 = InlineKeyboardButton('💌', url='t.me/Solevaaaya')
 markup_photo.row(bt1, bt3, bt2)
+
+
+markup_test = InlineKeyboardMarkup()
+b_test = InlineKeyboardButton('Пройти', callback_data='go')
+markup_test.row(b_test)
+
+
+markup_test_question = InlineKeyboardMarkup(row_width=2)
+bq1 = InlineKeyboardButton('Holdik', callback_data='holdik')
+bq2 = InlineKeyboardButton('VasyaDasher', callback_data='vasyadasher')
+bq3 = InlineKeyboardButton('MMA', callback_data='mma')
+bq4 = InlineKeyboardButton('Antinub', callback_data='antinub')
+b_test_cancel = InlineKeyboardButton('Отмена', callback_data='cancel')
+markup_test_question.row(bq1, bq2)
+markup_test_question.row(bq3, bq4)
+markup_test_question.row(b_test_cancel)
+
+
+markup_test_question2 = InlineKeyboardMarkup(row_width=2)
+bq5 = InlineKeyboardButton('20', callback_data='20')
+bq6 = InlineKeyboardButton('87', callback_data='87')
+bq7 = InlineKeyboardButton('16', callback_data='16')
+bq8 = InlineKeyboardButton('148', callback_data='148')
+markup_test_question2.row(bq5, bq6)
+markup_test_question2.row(bq7, bq8)
+markup_test_question2.row(b_test_cancel)
